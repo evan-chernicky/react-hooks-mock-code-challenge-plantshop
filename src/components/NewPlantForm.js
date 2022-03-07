7c9f4c8c7fa707b6e2915449bd653846b0330d04
Evan Chernicky <echernicky@gmail.com>
2022-03-06 21:11:50 -0500

    Assignment Complete

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,34 @@
 import React from "react";
 
-function NewPlantForm() {
+function NewPlantForm({renderPlant, plants}) {
+
+  function onNewPlant(e) {
+
+    e.preventDefault()
+
+    const newPlant = {
+      id: plants.length + 1,
+      name: e.target.name.value,
+      price: e.target.price.value,
+      image: e.target.image.value
+    }
+
+    renderPlant(newPlant)
+    
+    e.target.reset()
+
+
+
+  }
+
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
+      <form onSubmit={ (e) => onNewPlant(e)}>
         <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+        <input type="text" name="image"  placeholder="Image URL" />
+        <input type="number" name="price"  step="0.01" placeholder="Price" />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+  const [searchState, setSearchState] = useState('')
+
+
+
+  useEffect(() => {
+
+    fetch('http://localhost:6001/plants')
+    .then(resp => resp.json())
+    .then(plantData => setPlants(plantData))
+
+
+  }, [])
+
+  function renderPlant(newPlant) {
+    setPlants([...plants, newPlant])
+  }
+
+  function filterSearch(searchValue) {
+
+    setSearchState(searchValue) 
+
+  }
+
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm renderPlant={renderPlant} plants={plants} />
+      <Search filterSearch={filterSearch}/>
+      <PlantList plants={plants} searchState={searchState} />
     </main>
   );
 }
